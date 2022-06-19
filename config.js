d8ef4960f6faaf7fa1e914ca1cc17f4de0b92025
Yovani199912 <107808089+Yovani199912@users.noreply.github.com>
2022-06-19 14:49:59 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,63 @@
+global.DeveloperMode = 'false' //true Or false
+global.linkGC = ['https://chat.whatsapp.com/CG5ZPcJ22fL7QjNRzjguD0']
+global.channelYT = [] 
+global.owner = ['5212411347465']// Cambia los numeros por tu o tus numeros
+global.mods = ['5212377467'] // Cambia el numero y escribe el numero que sera del Bot
+global.prems = ['5212411719888'] // Cambia el numero y escribe el de un usuario premium VIP
+
+//Nota para ti xD: Puedes contactarme si necesitas ayuda con algo al +522411347465
+
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  amel: 'https://melcanz.com',
+  bx: 'https://bx-hunter.herokuapp.com',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  dzx: 'https://api.dhamzxploit.my.id',
+  bsbt: 'https://bsbt-api-rest.herokuapp.com',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz', 
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  lol: 'https://api.lolhuman.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  Velgrynd: 'https://velgrynd.herokuapp.com',
+  hardianto: 'http://hardianto-chan.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://melcanz.com': 'DyotaMC05',
+  'https://bx-hunter.herokuapp.com': 'Ikyy69',
+  'https://api.xteam.xyz': '5bd33b276d41d6b4',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://bsbt-api-rest.herokuapp.com': 'benniismael',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://pencarikode.xyz': 'pais', 
+  'https://leyscoders-api.herokuapp.com': 'MIMINGANZ', 
+  'https://api.lolhuman.xyz': 'HIRO'
+}
+
+// Sticker WM
+global.packname = '❍͜͡➣𝐘𝐎𝐕𝐀𝐍𝐈❍͜͡➣'
+global.author = '❍͜͡➣𝐇𝐀𝐃𝐄𝐒_𝐁𝐎𝐓❍͜͡➣'
+
+
+//global.wait = '*Esperé un momento..*'
+
+
+global.multiplier = 69 // The higher, The harder levelup
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
